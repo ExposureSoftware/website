15ab5b7282e6a4bb7acc3af5ebab07f8ab1edadb
Marshall Davis <toothlessrebel@gmail.com>
2020-12-31 07:19:49 -0500

    Correct CodeDeploy script

--- codeDeploy/afterInstall.sh
+++ codeDeploy/afterInstall.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-aws s3 cp s3://exposure-configs/exposure.env /usr/share/nginx/exposure/.env
+aws s3 cp s3://exposuresoftware-configs/exposure.env /usr/share/nginx/exposure/.env
 cd /usr/share/nginx/exposure || exit 1
 su www-data -s /bin/bash -c "php artisan migrate --force"
 aws s3 cp s3://exposuresoftware-configs/exposure.conf /etc/nginx/sites-enabled/exposure.conf
