06a5139b6eacbc198d6b083434d432c2982e4c8e
Marshall Davis <toothlessrebel@gmail.com>
2021-06-18 20:24:04 -0400

    Run NPM twice

--- webpack.mix.js
+++ webpack.mix.js
@@ -17,19 +17,20 @@ mix.js('resources/js/app.js', 'public/js').vue()
             'resources/js/wow.min.js',
         ],
         'public/js/prereqs.js',
-    ).less('resources/less/app.less', 'public/css/app.css')
-    .sass('resources/scss/style.scss', 'public/css/style.css');
-mix.styles([
-    'resources/css/slick.css',
-    'resources/css/font-awesome.css',
-    'resources/css/LineIcons.css',
-    'resources/css/animate.css',
-    'resources/css/magnific-popup.css',
-    'resources/css/bootstrap.min.css',
-    'resources/css/default.css',
-    'public/css/style.css',
-    'public/css/app.css',
-], 'public/css/all.css');
+    )
+    .less('resources/less/app.less', 'public/css/app.css')
+    .sass('resources/scss/style.scss', 'public/css/style.css')
+    .styles([
+        'resources/css/slick.css',
+        'resources/css/font-awesome.css',
+        'resources/css/LineIcons.css',
+        'resources/css/animate.css',
+        'resources/css/magnific-popup.css',
+        'resources/css/bootstrap.min.css',
+        'resources/css/default.css',
+        'public/css/style.css',
+        'public/css/app.css',
+    ], 'public/css/all.css');
 
 if (mix.inProduction()) {
     mix.version();
